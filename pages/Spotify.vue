55b5f4d93c04b0a991e9f282f3fd42c00407b81d
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2023-07-22 20:54:56 -0700

    capitalize

--- pages/Spotify.vue
+++ pages/Spotify.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto min-h-screen p-10 md:w-2/3">
     <div class="flex flex-row items-center justify-center">
-      <img class="flex h-28 flex-col" alt="google maps icon" src="~/assets/images/spotify/logo.png" />
+      <img class="flex h-28 flex-col" alt="google maps icon" src="~/assets/images/Spotify/logo.png" />
       <p class="flex flex-col text-center text-4xl font-bold tracking-tight md:text-7xl">Spotify Playlist Analyzer</p>
     </div>
     <p class="mt-2 text-center text-xl tracking-tighter md:text-2xl">
